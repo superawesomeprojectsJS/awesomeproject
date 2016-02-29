0857329fbca39e827b7b4f0d2bc3f0bf14cc39bc
marcusmellis89 <m.m.ellis89@gmail.com>
2016-02-29 02:48:42 -0800

    [progress]authentication scaffolding

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8000;
 
@@ -6,6 +7,8 @@ var PORT = process.env.PORT || 8000;
 // app.use('/', function(req, res){
 //   res.send('hello world');
 // })
+// console.log(path.join(__dirname,'../index.html'));
+app.use(express.static(path.join(__dirname,'../')));
 
 app.listen(PORT);
 console.log('listening on 8000...')
