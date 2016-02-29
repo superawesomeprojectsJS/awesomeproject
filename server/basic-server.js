d205f2e44cd070e86cc45c935d4885ed66fc093a
chelsea cheung <chelseatcheung@gmail.com>
2016-02-29 15:57:54 -0800

    [progress] connecting mongoDB

--- server/basic-server.js
+++ server/basic-server.js
@@ -2,13 +2,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8000;
+var connection = require('./connections.js');
 
-
-// app.use('/', function(req, res){
-//   res.send('hello world');
-// })
-// console.log(path.join(__dirname,'../index.html'));
-app.use(express.static(path.join(__dirname,'../')));
+app.use(express.static(path.join(__dirname,"../")));
 
 app.listen(PORT);
 console.log('listening on 8000...')
